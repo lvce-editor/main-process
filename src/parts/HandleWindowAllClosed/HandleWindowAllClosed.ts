bd6837c1c5770b7344af7ec5c887f0af52762777
Le Vivilet <levivlet@pm.me>
2024-12-23 10:12:34 +0100

    feature: improve lint

--- src/parts/HandleWindowAllClosed/HandleWindowAllClosed.ts
+++ src/parts/HandleWindowAllClosed/HandleWindowAllClosed.ts
@@ -1,5 +1,5 @@
 import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
 
 export const handleWindowAllClosed = async () => {
-  SharedProcess.send('HandleWindowAllClosed.handleWindowAllClosed')
+  await SharedProcess.send('HandleWindowAllClosed.handleWindowAllClosed')
 }
