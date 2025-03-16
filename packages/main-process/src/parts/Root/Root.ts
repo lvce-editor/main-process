cb0509b3f31a917975799703b3e46f63e79ce29b
Le Vivilet <levivlet@pm.me>
2025-03-16 15:24:52 +0100

    feature: use path.join helper function

--- packages/main-process/src/parts/Root/Root.ts
+++ packages/main-process/src/parts/Root/Root.ts
@@ -1,6 +1,7 @@
-import { dirname, join } from 'node:path'
+import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import * as Path from '../Path/Path.ts'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-export const root = process.env.LVCE_ROOT || join(__dirname, '../../../../..')
+export const root = process.env.LVCE_ROOT || Path.join(__dirname, '../../../../..')
