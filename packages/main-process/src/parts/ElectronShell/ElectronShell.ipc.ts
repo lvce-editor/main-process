34264e924b1078e36f69c4c158bf924633549163
Le Vivilet <levivlet@pm.me>
2025-04-11 19:05:19 +0200

    remove deprecated code

--- packages/main-process/src/parts/ElectronShell/ElectronShell.ipc.ts
+++ packages/main-process/src/parts/ElectronShell/ElectronShell.ipc.ts
@@ -1,10 +1,12 @@
+import * as Beep from '../Beep/Beep.ts'
+import * as OpenExternal from '../OpenExternal/OpenExternal.ts'
 import * as ElectronShell from './ElectronShell.ts'
 
 export const name = 'ElectronShell'
 
 export const Commands = {
-  beep: ElectronShell.beep,
-  openExternal: ElectronShell.openExternal,
+  beep: Beep.beep,
+  openExternal: OpenExternal.openExternal,
   openPath: ElectronShell.showItemInFolder,
   showItemInFolder: ElectronShell.showItemInFolder,
 }
