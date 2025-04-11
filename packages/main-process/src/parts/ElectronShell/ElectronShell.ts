979c42ba9d22638b42af0345c485bca883368194
Le Vivilet <levivlet@pm.me>
2025-04-11 19:05:37 +0200

    simplify code

--- packages/main-process/src/parts/ElectronShell/ElectronShell.ts
+++ packages/main-process/src/parts/ElectronShell/ElectronShell.ts
@@ -1,24 +1,10 @@
 import { shell } from 'electron'
-import * as OpenExternal from '../OpenExternal/OpenExternal.ts'
 
 export const showItemInFolder = (fullPath) => {
   shell.showItemInFolder(fullPath)
 }
 
-/**
- * @deprecated use OpenExternal.openExternal function instead
- */
-export const { openExternal } = OpenExternal
-
 export const openPath = async (path) => {
   // TODO handle error
   await shell.openPath(path)
-}
-
-/**
- * @deprecated use Beep.beep instead
- */
-export const beep = async () => {
-  const Beep = await import('../Beep/Beep.ts')
-  Beep.beep()
 }
