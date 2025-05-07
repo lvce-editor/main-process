536f219176eaefd84802e9aa90db66927af401d6
Le Vivilet <levivlet@pm.me>
2025-05-07 20:31:13 +0200

    feature: add more commands to command map

--- packages/main-process/src/parts/CommandMap/CommandMap.ts
+++ packages/main-process/src/parts/CommandMap/CommandMap.ts
@@ -1,21 +1,29 @@
 import * as AppWindow from '../AppWindow/AppWindow.ts'
 import * as Beep from '../Beep/Beep.ts'
 import * as Crash from '../Crash/Crash.ts'
+import * as CreateMessagePort from '../CreateMessagePort/CreateMessagePort.ts'
 import * as CreatePidMap from '../CreatePidMap/CreatePidMap.ts'
+import * as DesktopCapturer from '../DesktopCapturer/DesktopCapturer.ts'
+import * as ElectronApplicationMenu from '../ElectronApplicationMenu/ElectronApplicationMenu.ts'
 import * as ElectronBeep from '../ElectronBeep/ElectronBeep.ts'
 import * as ElectronContentTracing from '../ElectronContentTracing/ElectronContentTracing.ts'
 import * as ElectronDeveloper from '../ElectronDeveloper/ElectronDeveloper.ts'
 import * as ElectronDialog from '../ElectronDialog/ElectronDialog.ts'
+import * as ElectronNet from '../ElectronNet/ElectronNet.ts'
 import * as ElectronNetLog from '../ElectronNetLog/ElectronNetLog.ts'
 import * as ElectronPowerSaveBlocker from '../ElectronPowerSaveBlocker/ElectronPowerSaveBlocker.ts'
 import * as ElectronSafeStorage from '../ElectronSafeStorage/ElectronSafeStorage.ts'
 import * as ElectronScreen from '../ElectronScreen/ElectronScreen.ts'
+import * as ElectronSession from '../ElectronSession/ElectronSession.ts'
 import * as ElectronShell from '../ElectronShell/ElectronShell.ts'
+import * as ElectronWebContentsView from '../ElectronWebContentsView/ElectronWebContentsView.ts'
+import * as ElectronWebContentsViewFunctions from '../ElectronWebContentsViewFunctions/ElectronWebContentsViewFunctions.ts'
 import * as ElectronWindow from '../ElectronWindow/ElectronWindow.ts'
 import * as ElectronWindowProcessExplorer from '../ElectronWindowProcessExplorer/ElectronWindowProcessExplorer.ts'
 import * as Exit from '../Exit/Exit.ts'
 import * as GetWindowId from '../GetWindowId/GetWindowId.ts'
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
+import * as IpcParent from '../IpcParent/IpcParent.ts'
 import * as OpenExternal from '../OpenExternal/OpenExternal.ts'
 import * as Process from '../Process/Process.ts'
 import * as TemporaryMessagePort from '../TemporaryMessagePort/TemporaryMessagePort.ts'
@@ -25,6 +33,10 @@ export const commandMap = {
   'AppWindow.createAppWindow': AppWindow.createAppWindow,
   'Beep.beep': Beep.beep,
   'Crash.crashMainProcess': Crash.crashMainProcess,
+  'DesktopCapturer.getSources': DesktopCapturer.getSources,
+  'CreateMessagePort.createMessagePort': CreateMessagePort.createMessagePort,
+  'CreatePidMap.createPidMap': CreatePidMap.createPidMap,
+  'ElectronApplicationMenu.setItems': ElectronApplicationMenu.setItems,
   'ElectronBeep.beep': ElectronBeep.beep,
   'ElectronContentTracing.startRecording': ElectronContentTracing.startRecording,
   'ElectronContentTracing.stopRecording': ElectronContentTracing.stopRecording,
@@ -32,6 +44,7 @@ export const commandMap = {
   'ElectronDeveloper.getPerformanceEntries': ElectronDeveloper.getPerformanceEntries,
   'ElectronDialog.showMessageBox': ElectronDialog.showMessageBox,
   'ElectronDialog.showOpenDialog': ElectronDialog.showOpenDialog,
+  'ElectronNet.getJson': ElectronNet.getJson,
   'ElectronNetLog.startLogging': ElectronNetLog.startLogging,
   'ElectronNetLog.stopLogging': ElectronNetLog.stopLogging,
   'ElectronPowerSaveBlocker.start': ElectronPowerSaveBlocker.start,
@@ -39,16 +52,43 @@ export const commandMap = {
   'ElectronSafeStorage.decrypt': ElectronSafeStorage.decrypt,
   'ElectronSafeStorage.encrypt': ElectronSafeStorage.encrypt,
   'ElectronSafeStorage.isEncryptionAvailable': ElectronSafeStorage.isEncryptionAvailable,
+  'ElectronScreen.getBounds': ElectronScreen.getBounds,
+  'ElectronScreen.getHeight': ElectronScreen.getHeight,
+  'ElectronScreen.getWidth': ElectronScreen.getWidth,
+  'ElectronSession.registerWebviewProtocol': ElectronSession.registerWebviewProtocol,
   'ElectronShell.beep': ElectronBeep.beep,
   'ElectronShell.openExternal': OpenExternal.openExternal,
   'ElectronShell.openPath': ElectronShell.openPath,
   'ElectronShell.showItemInFolder': ElectronShell.showItemInFolder,
+  'ElectronWebContentsView.attachEventListeners': ElectronWebContentsView.attachEventListeners,
+  'ElectronWebContentsView.createWebContentsView': ElectronWebContentsView.createWebContentsView,
+  'ElectronWebContentsView.disposeWebContentsView': ElectronWebContentsView.disposeWebContentsView,
+  'ElectronWebContentsViewFunctions.addToWindow': ElectronWebContentsViewFunctions.addToWindow,
+  'ElectronWebContentsViewFunctions.backward': ElectronWebContentsViewFunctions.backward,
+  'ElectronWebContentsViewFunctions.cancelNavigation': ElectronWebContentsViewFunctions.cancelNavigation,
+  'ElectronWebContentsViewFunctions.copyImageAt': ElectronWebContentsViewFunctions.copyImageAt,
+  'ElectronWebContentsViewFunctions.focus': ElectronWebContentsViewFunctions.focus,
+  'ElectronWebContentsViewFunctions.forward': ElectronWebContentsViewFunctions.forward,
+  'ElectronWebContentsViewFunctions.getStats': ElectronWebContentsViewFunctions.getStats,
+  'ElectronWebContentsViewFunctions.hide': ElectronWebContentsViewFunctions.hide,
+  'ElectronWebContentsViewFunctions.inspectElement': ElectronWebContentsViewFunctions.inspectElement,
+  'ElectronWebContentsViewFunctions.openDevtools': ElectronWebContentsViewFunctions.openDevtools,
+  'ElectronWebContentsViewFunctions.reload': ElectronWebContentsViewFunctions.reload,
+  'ElectronWebContentsViewFunctions.resizeBrowserView': ElectronWebContentsViewFunctions.resizeBrowserView,
+  'ElectronWebContentsViewFunctions.setBackgroundColor': ElectronWebContentsViewFunctions.setBackgroundColor,
+  'ElectronWebContentsViewFunctions.setFallthroughKeyBindings': ElectronWebContentsViewFunctions.setFallthroughKeyBindings,
+  'ElectronWebContentsViewFunctions.setIframeSrc': ElectronWebContentsViewFunctions.setIframeSrc,
+  'ElectronWebContentsViewFunctions.setIframeSrcFallback': ElectronWebContentsViewFunctions.setIframeSrcFallback,
+  'ElectronWebContentsViewFunctions.show': ElectronWebContentsViewFunctions.show,
   'ElectronWindow.executeWebContentsFunction': ElectronWindow.executeWebContentsFunction,
   'ElectronWindow.executeWindowFunction': ElectronWindow.executeWindowFunction,
   'ElectronWindow.getFocusedWindowId': ElectronWindow.getFocusedWindowId,
   'ElectronWindow.getZoom': ElectronWindow.getZoom,
   'ElectronWindowProcessExplorer.open2': ElectronWindowProcessExplorer.open2,
-  'CreatePidMap.createPidMap': CreatePidMap.createPidMap,
+  'Exit.exit': Exit.exit,
+  'GetWindowId.getWindowId': GetWindowId.getWindowId,
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
+  'IpcParent.create': IpcParent.create,
   'OpenExternal.openExternal': OpenExternal.openExternal,
   'Process.getArgv': Process.getArgv,
   'Process.getChromeVersion': Process.getChromeVersion,
@@ -56,14 +96,8 @@ export const commandMap = {
   'Process.getNodeVersion': Process.getNodeVersion,
   'Process.getPid': Process.getPid,
   'Process.getV8Version': Process.getV8Version,
-  'Exit.exit': Exit.exit,
   'TemporaryMessagePort.createPortTuple': TemporaryMessagePort.createPortTuple,
   'TemporaryMessagePort.dispose': TemporaryMessagePort.dispose,
   'TemporaryMessagePort.sendTo': TemporaryMessagePort.sendTo,
   'Trash.trash': Trash.trash,
-  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
-  'ElectronScreen.getBounds': ElectronScreen.getBounds,
-  'ElectronScreen.getWidth': ElectronScreen.getWidth,
-  'ElectronScreen.getHeight': ElectronScreen.getHeight,
-  'GetWindowId.getWindowId': GetWindowId.getWindowId,
 }
