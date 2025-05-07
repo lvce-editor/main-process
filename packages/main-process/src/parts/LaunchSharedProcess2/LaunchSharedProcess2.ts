42b58f3c85ec1604613682dfbae013cd2f5b8f8c
Le Vivilet <levivlet@pm.me>
2025-05-07 21:00:07 +0200

    try to improve rpc

--- packages/main-process/src/parts/LaunchSharedProcess2/LaunchSharedProcess2.ts
+++ packages/main-process/src/parts/LaunchSharedProcess2/LaunchSharedProcess2.ts
@@ -1,6 +1,11 @@
 import { ElectronUtilityProcessRpcParent } from '@lvce-editor/rpc'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
+import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import * as GetSharedProcessArgv from '../GetSharedProcessArgv/GetSharedProcessArgv.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
+import * as IpcChild from '../IpcChild/IpcChild.ts'
+import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
+import * as IpcId from '../IpcId/IpcId.ts'
 import * as Performance from '../Performance/Performance.ts'
 import * as PerformanceMarkerType from '../PerformanceMarkerType/PerformanceMarkerType.ts'
 import * as Platform from '../Platform/Platform.ts'
@@ -20,9 +25,18 @@ export const launchSharedProcess2 = async ({ method, env = {} }) => {
     execArgv,
     path: sharedProcessPath,
     name: 'shared-process',
-    commandMap: CommandMap.commandMap,
+    commandMap: CommandMapRef.commandMapRef,
     // @ts-ignore
     requiresSocket: RequiresSocket.requiresSocket,
   })
+
+  const { port1, port2 } = GetPortTuple.getPortTuple()
+  const childIpc = await IpcChild.listen({
+    method: IpcChildType.ElectronMessagePort,
+    messagePort: port1,
+  })
+  HandleIpc.handleIpc(childIpc)
+  await sharedProcess.invokeAndTransfer('HandleElectronMessagePort.handleElectronMessagePort', port2, IpcId.MainProcess)
+
   return sharedProcess
 }
--- packages/main-process/src/parts/Main/Main.ts
+++ packages/main-process/src/parts/Main/Main.ts
@@ -6,8 +6,11 @@ import * as ErrorHandling from '../ErrorHandling/ErrorHandling.ts'
 import * as Module from '../Module/Module.ts'
 import * as Process from '../Process/Process.ts'
 import * as SetStackTraceLimit from '../SetStackTraceLimit/SetStackTraceLimit.ts'
+import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 
 export const main = async () => {
+  Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   SetStackTraceLimit.setStackTraceLimit(20)
   Process.on('uncaughtExceptionMonitor', ErrorHandling.handleUncaughtExceptionMonitor)
   // workaround for https://github.com/electron/electron/issues/36526
