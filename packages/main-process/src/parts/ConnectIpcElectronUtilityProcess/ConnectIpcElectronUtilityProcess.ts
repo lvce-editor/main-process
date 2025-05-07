97cfff965ac6eb2f6867bbb37859de69db858d52
Le Vivilet <levivlet@pm.me>
2025-05-07 22:29:18 +0200

    feature: use rpc module more frequently

--- packages/main-process/src/parts/ConnectIpcElectronUtilityProcess/ConnectIpcElectronUtilityProcess.ts
+++ packages/main-process/src/parts/ConnectIpcElectronUtilityProcess/ConnectIpcElectronUtilityProcess.ts
@@ -1,5 +1,5 @@
 import * as TransferMessagePortMain from '../TransferMessagePortMain/TransferMessagePortMain.ts'
 
-export const connectIpc = async (ipc, browserWindowPort, ...params) => {
-  await TransferMessagePortMain.transferMessagePortMain(ipc, browserWindowPort, ...params)
+export const connectIpc = async (rpc, browserWindowPort, ...params) => {
+  await TransferMessagePortMain.transferMessagePortMain(rpc, browserWindowPort, ...params)
 }
--- packages/main-process/src/parts/TransferMessagePortMain/TransferMessagePortMain.ts
+++ packages/main-process/src/parts/TransferMessagePortMain/TransferMessagePortMain.ts
@@ -1,9 +1,8 @@
-import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
 import { VError } from '../VError/VError.ts'
 
-export const transferMessagePortMain = async (ipc, port, ...params) => {
+export const transferMessagePortMain = async (rpc, port, ...params) => {
   try {
-    await JsonRpc.invokeAndTransfer(ipc, 'HandleElectronMessagePort.handleElectronMessagePort', port, ...params)
+    await rpc.invokeAndTransfer('HandleElectronMessagePort.handleElectronMessagePort', port, ...params)
   } catch (error) {
     throw new VError(error, `Failed to send message port to utility process`)
   }
