60269a8d967ebcdb0c5731d731051d1cd11fa79a
Le Vivilet <levivlet@pm.me>
2025-05-07 22:13:36 +0200

    feature: eagerly load ipc since it is bundled anyway

--- packages/main-process/src/parts/ConnectIpc/ConnectIpc.ts
+++ packages/main-process/src/parts/ConnectIpc/ConnectIpc.ts
@@ -1,17 +1,19 @@
+import * as ConnectIpcElectronUtilityProcess from '../ConnectIpcElectronUtilityProcess/ConnectIpcElectronUtilityProcess.ts'
+import * as ConnectIpcNodeWorker from '../ConnectIpcNodeWorker/ConnectIpcNodeWorker.ts'
 import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 
 const getModule = (method: number): any => {
   switch (method) {
     case IpcParentType.NodeWorker:
-      return import('../ConnectIpcNodeWorker/ConnectIpcNodeWorker.ts')
+      return ConnectIpcNodeWorker.connectIpc
     case IpcParentType.ElectronUtilityProcess:
-      return import('../ConnectIpcElectronUtilityProcess/ConnectIpcElectronUtilityProcess.ts')
+      return ConnectIpcElectronUtilityProcess.connectIpc
     default:
       throw new Error('unexpected ipc type')
   }
 }
 
 export const connectIpc = async (method, ipc, browserWindowPort, ipcId): Promise<any> => {
-  const module = await getModule(method)
-  return module.connectIpc(ipc, browserWindowPort, ipcId)
+  const connectIpc = getModule(method)
+  return connectIpc(ipc, browserWindowPort, ipcId)
 }
