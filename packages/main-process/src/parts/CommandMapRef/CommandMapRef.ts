fc8c0c7e26d306405a7d49c51345f6a7b9e352f1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-07 21:14:35 +0200

    feature: make use of commandmap (#106)
    * feature: make use of commandmap
    
    * lint

---
+++ packages/main-process/src/parts/CommandMapRef/CommandMapRef.ts
@@ -0,0 +1,1 @@
+export const commandMapRef = Object.create(null)
--- packages/main-process/src/parts/Main/Main.ts
+++ packages/main-process/src/parts/Main/Main.ts
@@ -2,12 +2,15 @@
 performance.mark('code/start')
 import * as App from '../App/App.ts'
 import * as Command from '../Command/Command.ts'
+import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 import * as ErrorHandling from '../ErrorHandling/ErrorHandling.ts'
 import * as Module from '../Module/Module.ts'
 import * as Process from '../Process/Process.ts'
 import * as SetStackTraceLimit from '../SetStackTraceLimit/SetStackTraceLimit.ts'
 
 export const main = async () => {
+  Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   SetStackTraceLimit.setStackTraceLimit(20)
   Process.on('uncaughtExceptionMonitor', ErrorHandling.handleUncaughtExceptionMonitor)
   // workaround for https://github.com/electron/electron/issues/36526
