4a8db0023aaaf4121af62d4841cc8282dd827d76
Le Vivilet <levivlet@pm.me>
2025-05-08 17:42:55 +0200

    feature: use rpc for webview protocol

--- packages/main-process/src/parts/ElectronSession/ElectronSession.ts
+++ packages/main-process/src/parts/ElectronSession/ElectronSession.ts
@@ -1,10 +1,9 @@
+import { ElectronMessagePortRpcClient } from '@lvce-editor/rpc'
 import * as Assert from '../Assert/Assert.ts'
 import * as CreateElectronSession from '../CreateElectronSession/CreateElectronSession.ts'
 import * as ElectronSessionState from '../ElectronSessionState/ElectronSessionState.ts'
-import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
-import * as IpcParent from '../IpcParent/IpcParent.ts'
-import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 import * as Protocol from '../Protocol/Protocol.ts'
+import * as RequiresSocket from '../RequiresSocket/RequiresSocket.ts'
 import * as Scheme from '../Scheme/Scheme.ts'
 import * as WebViewRequestHandler from '../WebViewRequestHandler/WebViewRequestHandler.ts'
 
@@ -17,16 +16,12 @@ export const get = (): globalThis.Electron.Session => {
 
 export const registerWebviewProtocol = async (port) => {
   Assert.object(port)
-  console.log('will register ptorocol')
-  const ipc = await IpcParent.create({
-    method: IpcParentType.ElectronMessagePort,
+  const rpc = await ElectronMessagePortRpcClient.create({
+    commandMap: {},
     messagePort: port,
+    requiresSocket: RequiresSocket.requiresSocket,
   })
-  console.log('do register protocol')
-  HandleIpc.handleIpc(ipc)
-  port.start()
   const session = get()
-  const handleRequest = WebViewRequestHandler.create(ipc)
-  // @ts-ignore
+  const handleRequest = WebViewRequestHandler.create(rpc)
   Protocol.handle(session.protocol, Scheme.WebView, handleRequest)
 }
--- packages/main-process/src/parts/WebViewRequestHandler/WebViewRequestHandler.ts
+++ packages/main-process/src/parts/WebViewRequestHandler/WebViewRequestHandler.ts
@@ -1,9 +1,9 @@
-import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
+import type { Rpc } from '@lvce-editor/rpc'
 
-export const create = (ipc) => {
+export const create = (rpc: Rpc) => {
   const handleRequest = async (request) => {
     const { method, url } = request
-    const { body, init } = await JsonRpc.invoke(ipc, 'WebViewProtocol.getResponse', method, url)
+    const { body, init } = await rpc.invoke('WebViewProtocol.getResponse', method, url)
     return new Response(body, init)
   }
   return handleRequest
