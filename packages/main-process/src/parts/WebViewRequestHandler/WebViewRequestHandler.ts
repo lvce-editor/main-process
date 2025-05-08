fbd825752fa7a9869e884a35a67bfced7f5819fd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-08 17:38:43 +0200

    fix: remove console log (#125)

--- packages/main-process/src/parts/WebViewRequestHandler/WebViewRequestHandler.ts
+++ packages/main-process/src/parts/WebViewRequestHandler/WebViewRequestHandler.ts
@@ -3,7 +3,6 @@ import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
 export const create = (ipc) => {
   const handleRequest = async (request) => {
     const { method, url } = request
-    console.log('got request', method, url)
     const { body, init } = await JsonRpc.invoke(ipc, 'WebViewProtocol.getResponse', method, url)
     return new Response(body, init)
   }
