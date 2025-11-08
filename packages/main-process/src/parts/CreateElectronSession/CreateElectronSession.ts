af84be9678ee4a5e23a3e45f0ef6a5c45d022ffe
Le Vivilet <levivilet@pm.me>
2025-11-08 19:46:40 +0100

    feature: allow workers to download github release assets for updates (#227)

--- packages/main-process/src/parts/CreateElectronSession/CreateElectronSession.ts
+++ packages/main-process/src/parts/CreateElectronSession/CreateElectronSession.ts
@@ -5,6 +5,39 @@ import * as IsSessionCacheEnabled from '../IsSessionCacheEnabled/IsSessionCacheE
 import * as Platform from '../Platform/Platform.ts'
 import * as Protocol from '../Protocol/Protocol.ts'
 
+// details: globalThis.Electron.OnBeforeSendHeadersListenerDetails, callback: (beforeSendResponse: globalThis.Electron.BeforeSendResponse
+
+const onBeforeSendHeadersCallback = (
+  details: Electron.OnBeforeSendHeadersListenerDetails,
+  callback: (beforeSendResponse: Electron.BeforeSendResponse) => void,
+) => {
+  if (details && details.requestHeaders) {
+    // @ts-ignore
+    details.requestHeaders['Origin'] = null
+    details.requestHeaders['Access-Control-Allow-Origin'] = '*'
+  }
+  // @ts-ignore
+  if (details && details.headers) {
+    // @ts-ignore
+    details.headers['Origin'] = null
+    // @ts-ignore
+    details.headers['Access-Control-Allow-Origin'] = '*'
+  }
+  callback({ requestHeaders: details.requestHeaders })
+}
+
+const onHeadersReceivedCallback = (
+  details: Electron.OnHeadersReceivedListenerDetails,
+  callback: (headersReceivedResponse: globalThis.Electron.HeadersReceivedResponse) => void,
+) => {
+  callback({
+    responseHeaders: {
+      'Access-Control-Allow-Origin': ['*'],
+      ...details.responseHeaders,
+    },
+  })
+}
+
 // TODO maybe create a separate session for webviews
 export const createElectronSession = (): globalThis.Electron.Session => {
   const sessionId = Platform.getSessionId()
@@ -13,6 +46,11 @@ export const createElectronSession = (): globalThis.Electron.Session => {
   })
   session.setPermissionRequestHandler(HandlePermission.handlePermissionRequest)
   session.setPermissionCheckHandler(HandlePermission.handlePermissionCheck)
+  const filter = {
+    urls: ['https://*.github.com/*', 'https://release-assets.githubusercontent.com/*'],
+  }
+  session.webRequest.onBeforeSendHeaders(filter, onBeforeSendHeadersCallback)
+  session.webRequest.onHeadersReceived(filter, onHeadersReceivedCallback)
   Protocol.handle(session.protocol, Platform.scheme, HandleRequest.handleRequest)
   return session
 }
