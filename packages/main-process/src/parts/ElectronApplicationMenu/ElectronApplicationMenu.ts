7c7f97578aa8cba1d8f86eaa6503b759b3d4fe27
Le Vivilet <levivilet@pm.me>
2026-01-04 22:02:55 +0100

    feature: add menu types

--- packages/main-process/src/parts/ElectronApplicationMenu/ElectronApplicationMenu.ts
+++ packages/main-process/src/parts/ElectronApplicationMenu/ElectronApplicationMenu.ts
@@ -2,15 +2,23 @@ import { Menu } from 'electron'
 import * as Assert from '../Assert/Assert.ts'
 import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
 
-export const setMenu = (menu) => {
+export const setMenu = (menu: Electron.Menu): void => {
   Menu.setApplicationMenu(menu)
 }
 
-const click = async (menuItem, browserWindow, keys) => {
+const click = async (menuItem: Electron.MenuItem, browserWindow: Electron.BaseWindow | undefined, keys) => {
+  if (!browserWindow) {
+    return
+  }
   await SharedProcess.send('ElectronApplicationMenu.handleClick', browserWindow.id, menuItem.label)
 }
 
-const addClickListener = (item) => {
+interface RawItem {
+  readonly label: string
+  readonly submenu?: readonly RawItem[]
+}
+
+const addClickListener = (item: RawItem): Electron.MenuItemConstructorOptions => {
   if (item.submenu) {
     return {
       ...item,
@@ -21,17 +29,18 @@ const addClickListener = (item) => {
   return {
     ...item,
     click,
+    submenu: undefined,
   }
 }
 
-export const setItems = (items) => {
+export const setItems = (items: readonly RawItem[]) => {
   Assert.array(items)
   const itemsWithClickListeners = items.map(addClickListener)
   const menu = Menu.buildFromTemplate(itemsWithClickListeners)
   setMenu(menu)
 }
 
-export const createTitleBar = (items) => {
+export const createTitleBar = (items: Electron.MenuItem[]) => {
   const menuBar = Menu.buildFromTemplate(items)
   return menuBar
 }
