73353d048cdc18e083c1efadc3b463b667a19527
Le Vivilet <levivilet@pm.me>
2026-01-07 18:27:37 +0100

    feature: validate url

--- packages/main-process/src/parts/OpenExternal/OpenExternal.ts
+++ packages/main-process/src/parts/OpenExternal/OpenExternal.ts
@@ -1,8 +1,10 @@
 import { shell } from 'electron'
+import * as Assert from '../Assert/Assert.ts'
 import * as ShouldOpenExternal from '../ShouldOpenExternal/ShouldOpenExternal.ts'
 import { VError } from '../VError/VError.ts'
 
-export const openExternal = async (url) => {
+export const openExternal = async (url: string): Promise<void> => {
+  Assert.string(url)
   if (!ShouldOpenExternal.shouldOpenExternal(url)) {
     throw new VError(`only http or https urls are allowed`)
   }
