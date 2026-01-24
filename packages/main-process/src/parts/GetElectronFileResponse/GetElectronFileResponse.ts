f3435da13bd589099ae7e8e0d8722123222d26e6
Le Vivilet <levivilet@pm.me>
2026-01-24 01:06:40 +0100

    feature: make configurable whether or not response should come from ipc (#262)

--- packages/main-process/src/parts/GetElectronFileResponse/GetElectronFileResponse.ts
+++ packages/main-process/src/parts/GetElectronFileResponse/GetElectronFileResponse.ts
@@ -1,12 +1,9 @@
-import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
+import { getElectronFileResponseIpc } from '../GetElectronFileResponseIpc/GetElectronFileResponseIpc.ts'
 
-/**
- *
- * @param {string} url
- * @param {any} request
- */
-export const getElectronFileResponse = async (url, request) => {
-  const { body, init } = await SharedProcess.invoke('GetElectronFileResponse.getElectronFileResponse', url, request)
-  const response = new Response(body, init)
-  return response
+export const getElectronFileResponse = async (url: string, request: any): Promise<Response> => {
+  const useIpc = true
+  if (useIpc) {
+    return getElectronFileResponseIpc(url, request)
+  }
+  throw new Error(`unsupported`)
 }
