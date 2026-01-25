c5af8fa642edcb45235f3eadc32bca0419d4db5f
Le Vivilet <levivilet@pm.me>
2026-01-26 00:20:43 +0100

    feature: open file as blob

--- packages/main-process/src/parts/GetFileResponse/GetFileResponse.ts
+++ packages/main-process/src/parts/GetFileResponse/GetFileResponse.ts
@@ -1,10 +1,10 @@
-import { readFileSync } from 'node:fs'
+/* eslint-disable n/no-unsupported-features/node-builtins */
+import { openAsBlob } from 'node:fs'
 
 export const getFileResponse = async (absolutePath: string, headers: any): Promise<Response> => {
   // TODO support request method head
-  // TODO maybe use readAsBlob
-  const content = readFileSync(absolutePath, 'utf8')
-  const response = new Response(content, {
+  const blob = await openAsBlob(absolutePath)
+  const response = new Response(blob, {
     headers,
     status: 200,
   })
