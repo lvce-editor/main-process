a57094d5e02f6174e8680b14f76cc7796c8a5738
Le Vivilet <levivilet@pm.me>
2026-01-26 00:25:45 +0100

    feature: add types

--- packages/main-process/src/parts/HandleRequest/HandleRequest.ts
+++ packages/main-process/src/parts/HandleRequest/HandleRequest.ts
@@ -1,11 +1,7 @@
 import * as GetElectronFileResponse from '../GetElectronFileResponse/GetElectronFileResponse.ts'
 import * as SerializeRequest from '../SerializeRequest/SerializeRequest.ts'
 
-/**
- *
- * @param {GlobalRequest} request
- */
-export const handleRequest = (request) => {
+export const handleRequest = (request: GlobalRequest): Promise<GlobalResponse> => {
   const serialized = SerializeRequest.serializeRequest(request)
   return GetElectronFileResponse.getElectronFileResponse(request.url, serialized)
 }
