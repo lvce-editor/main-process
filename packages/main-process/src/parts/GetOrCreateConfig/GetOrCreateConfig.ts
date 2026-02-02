0648536729c06a5e1960e215f381392be79aad16
Le Vivilet <levivilet@pm.me>
2026-02-02 18:13:28 +0100

    tsc

--- packages/main-process/src/parts/GetOrCreateConfig/GetOrCreateConfig.ts
+++ packages/main-process/src/parts/GetOrCreateConfig/GetOrCreateConfig.ts
@@ -2,16 +2,23 @@ import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { root } from '../Root/Root.ts'
 
-let config
+let config: ParsedConfig | undefined
 
-const createConfig = () => {
+type Headers = Record<string, string>
+
+export interface ParsedConfig {
+  readonly files: Record<string, number>
+  readonly headers: readonly Headers[]
+}
+
+const createConfig = (): ParsedConfig => {
   const configPath = join(root, 'config.json')
   const config = readFileSync(configPath, 'utf8')
   const parsedConfig = JSON.parse(config)
   return parsedConfig
 }
 
-export const getOrCreateConfig = () => {
+export const getOrCreateConfig = (): ParsedConfig => {
   if (!config) {
     config = createConfig()
   }
