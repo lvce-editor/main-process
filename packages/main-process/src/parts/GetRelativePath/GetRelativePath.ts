47f232a020636f6c9436d0cddfe424bfa9bf6bda
Le Vivilet <levivilet@pm.me>
2026-08-01 17:16:02 +0000

    fix: serve app urls with query parameters

--- packages/main-process/src/parts/GetRelativePath/GetRelativePath.ts
+++ packages/main-process/src/parts/GetRelativePath/GetRelativePath.ts
@@ -2,5 +2,6 @@ import { scheme } from '../Platform/Platform.ts'
 
 export const getRelativePath = (url: string) => {
   const relative = url.slice(scheme.length + 4)
-  return relative
+  const queryIndex = relative.indexOf('?')
+  return queryIndex === -1 ? relative : relative.slice(0, queryIndex)
 }
