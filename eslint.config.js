2a4bec2cfbf840a0b143b98dbdcd70be94bbc418
Le Vivilet <levivilet@pm.me>
2026-08-01 11:38:22 -0700

    feature: update eslint dependencies (#354)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedNode,
   {
@@ -35,4 +36,4 @@ export default [
       '@cspell/spellchecker': 'off',
     },
   },
-]
+])
