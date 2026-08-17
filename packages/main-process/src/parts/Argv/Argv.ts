92705acc655a5c4944962d7c649768fd53d81525
Le Vivilet <levivilet@pm.me>
2026-08-17 11:06:09 +0000

    fix: preserve electron development entry path

--- packages/main-process/src/parts/Argv/Argv.ts
+++ packages/main-process/src/parts/Argv/Argv.ts
@@ -1,5 +1,12 @@
 export const { argv } = process
 
+const getInsertionIndex = (): number => {
+  if (argv[0].endsWith('dist/electron') || argv[0].endsWith('dist\\electron.exe')) {
+    return 2
+  }
+  return 1
+}
+
 export const prepend = (argumentsToPrepend: readonly string[]): void => {
-  argv.splice(1, 0, ...argumentsToPrepend)
+  argv.splice(getInsertionIndex(), 0, ...argumentsToPrepend)
 }
